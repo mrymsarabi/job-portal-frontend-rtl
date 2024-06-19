3693e9ee9f1aa2d0e31133a321f02f1e8aadd554
Maryam <mrymsarabi@gmail.com>
2024-06-19 23:40:09 +0330

    Signup - 1

--- job-portal-frontend/src/App.jsx
+++ job-portal-frontend/src/App.jsx
@@ -5,11 +5,13 @@ import { Route, Routes, Navigate } from 'react-router-dom';
 
 //Components:
 import Login from '/src/components/Login';
+import Signup from '/src/components/Signup';
 
 const App = () => {
   return (
     <Routes>
       <Route path='/login' element={<Login />} />
+      <Route path='/signup' element={<Signup />} />
       <Route path="/*" element={<Navigate to="/login" replace />} />
     </Routes>
   );
--- job-portal-frontend/src/components/Signup.jsx
+++ job-portal-frontend/src/components/Signup.jsx
@@ -1,9 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+//CSS:
+import styles from "/src/styles/Signup.module.css";
 
 const Signup = () => {
+    //Statess:
+    const [data, setData] = useState({
+        id: "",
+        first_name: "",
+        last_name: "",
+        user_name: "",
+        user_role: "admin",
+    });
+
+    //Handling input changes:
+    const changeHandler = (event) => {
+        setData({...data, [event.target.name]: event.target.value});
+    };
+
+    //Handling form submit:
+    const submitHandler = async() => {
+
+    };
     return (
-        <div>
-            
+        <div className={styles.page}>
+            <div className={styles.content}>
+                <div className={styles.img_container}>
+                    <img src='/src/assets/Login.jpg' />
+                </div>
+                <form onSubmit={submitHandler}>
+
+                </form>
+            </div>
         </div>
     );
 };
