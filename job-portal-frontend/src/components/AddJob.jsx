f8ea8191741e8440a116f4b664ed12d49b4bf38e
Maryam <mrymsarabi@gmail.com>
2024-08-31 22:22:38 +0330

    AddJob - SelectComponent

--- job-portal-frontend/src/components/AddJob.jsx
+++ job-portal-frontend/src/components/AddJob.jsx
@@ -9,6 +9,7 @@ import { addJobAPI } from '/src/apis/addJobAPI';
 //Components:
 import Navbar from "/src/components/Navbar";
 import SubmitButton from '/src/components/SubmitButton';
+import SelectComponent from "/src/components/Inputs/SelectComponent";
 
 //CSS:
 import styles from "/src/styles/AddJob.module.css";
@@ -29,8 +30,155 @@ const AddJob = () => {
         requirements: "",
         description: "",
         benefits: "",
-        
     });
+
+    const sectors = [
+        {
+            value: "Education and training",
+            label: "Education and training"
+        },
+        {
+            value: "Construction, repair and maintenance services",
+            label: "Construction, repair and maintenance services"
+        },
+        {
+            value: "Manufacturing industry",
+            label: "Manufacturing industry"
+        },
+        {
+            value: "Transport of goods and people",
+            label: "Transport of goods and people"
+        }, 
+        {
+            value: "Health",
+            label: "Health"
+        },
+        {
+            value: "Aerospace and defence",
+            label: "Aerospace and defence"
+        },
+        {
+            value: "Real estate",
+            label: "Real estate"
+        },
+        {
+            value: "Government and public administration",
+            label: "Government and public administration"
+        },
+        {
+            value: "Human Resources and recruitment",
+            label: "Human Resources and recruitment"
+        },
+        {
+            value: "Pharmaceuticals and biotechnology",
+            label: "Pharmaceuticals and biotechnology"
+        },
+        {
+            value: "NGOs and non-profit associations",
+            label: "NGOs and non-profit associations"
+        },
+        {
+            value: "Arts, Entertainment and Recreation",
+            label: "Arts, Entertainment and Recreation"
+        }, 
+        {
+            value: "Legal",
+            label: "Legal"
+        },
+        {
+            value: "Retail and wholesale trade",
+            label: "Retail and wholesale trade"
+        },
+        {
+            value: "Energy and Exploitation of natural resources",
+            label:"Energy and Exploitation of natural resources"
+        },
+        {
+            value: "Finance",
+            label: "Finance"
+        },
+        {
+            value: "Computer Science",
+            label: "Computer Science"
+        },
+        {
+            value: "Particular services",
+            label: "Particular services"
+        },
+        {
+            value: "Management and business consulting",
+            label: "Management and business consulting"
+        },
+        {
+            value: "Insurance",
+            label: "Insurance"
+        },
+        {
+            value: "Restoration",
+            label: "Restoration"
+        },
+        {
+            value: "Media and communication",
+            label: "Media and communication"
+        },
+        {
+            value: "Telecommunications",
+            label: "Telecommunications"
+        },
+        {
+            value: "Agriculture",
+            label: "Agriculture"
+        },
+        {
+            value: "Hospitality and Tourism",
+            label: "Hospitality and Tourism"
+        }
+    ];
+
+    //Handle Sector Select:
+    const handleSectorChange = (selectedOption) => {
+        setData({...data, sector: selectedOption});
+    };
+
+    const jobTypes = [
+        {
+            value: "Full-time",
+            label: "Full-time"
+        },
+        {
+            value: "Permanent",
+            label: "Permanent"
+        },
+        {
+            value: "Contract",
+            label: "Contract"
+        },
+        {
+            value: "Part-time",
+            label:"Part-time"
+        },
+        {
+            value: "Temporary",
+            label: "Temporary"
+        },
+        {
+            value: "Apprenticeship",
+            label: "Apprenticeship"
+        },
+        {
+            value: "Internship",
+            label: "Internship"
+        },
+        {
+            value: "Volunteer",
+            label: "Volunteer"
+        }
+    ];
+
+    //HandleJob Type Select:
+    const handleJobTypeChange = (selectedOption) => {
+        setData({...data, job_type: selectedOption});
+    };
 
     //Functions:
     //Handling input changes:
@@ -77,11 +225,11 @@ const AddJob = () => {
                             </div>
                             <div className={styles.fieldContainer}>
                                 <label>Sector</label>
-                                <input type='text' name='sector' value={data.sector} onChange={changeHandler} />
+                                <SelectComponent options={sectors} handleChange={handleSectorChange} width="350px" height="40px" />
                             </div>
                             <div className={styles.fieldContainer}>
                                 <label>Job Type</label>
-                                <input type='text' name='job_type' value={data.job_type} onChange={changeHandler} />
+                                <SelectComponent options={jobTypes} handleChange={handleJobTypeChange} width="350px" height="40px" />
                             </div>
                             <div className={styles.fieldContainer}>
                                 <label>Country, City</label>
