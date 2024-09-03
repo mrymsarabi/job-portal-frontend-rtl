ffaeb0ae82c0b39077c7d06d912f6477eb1e363c
Maryam <mrymsarabi@gmail.com>
2024-09-03 19:56:31 +0330

    jobApplicationAnswerAPI - 1

---
+++ job-portal-frontend/src/apis/jobApplicationAnswerAPI.jsx
@@ -0,0 +1,24 @@
+import axios from "axios";
+
+export const jobApplicationAnswerAPI = async(data, applicationId, token) => {
+    try {
+        const response = await axios.patch(`http://localhost:5000/job-applications/applications/${applicationId}/status`,
+            {
+                status: data.status,
+                message: data.message  
+            },
+            {
+                headers: {
+                    'Content-Type': 'application/json',
+                    Authorization: `Bearer ${token}`,
+                },
+            }
+        );
+
+        console.log(response);
+        return response;
+    } catch(error) {
+        console.log(error);
+        return {status: "error", error: error.response ? error.response.data.error : 'An error occurred'};
+    };
+};
