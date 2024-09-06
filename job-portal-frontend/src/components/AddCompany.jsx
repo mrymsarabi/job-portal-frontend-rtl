b7e0cf21e33572af4b7ec152b3af2a8a20897fe1
Maryam <mrymsarabi@gmail.com>
2024-09-06 20:35:13 +0330

    AddCompany - finishing touches

--- job-portal-frontend/src/components/AddCompany.jsx
+++ job-portal-frontend/src/components/AddCompany.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 
 //Modules and Libraries:
 import Cookies from 'js-cookie';
+import { useNavigate } from 'react-router-dom';
 
 //APIs:
 import { addCompanyAPI } from '/src/apis/addCompanyAPI';
@@ -9,12 +10,17 @@ import { addCompanyAPI } from '/src/apis/addCompanyAPI';
 //Components:
 import Navbar from "/src/components/Navbar";
 import SubmitButton from "/src/components/SubmitButton";
+//Modals:
+import SuccessModal from "/src/components/Modals/SuccessModal";
+import UnsuccessModal from "/src/components/Modals/UnsuccessModal";
 
 //CSS:
 import styles from "/src/styles/AddCompany.module.css";
 
 const AddCompany = () => {
     const token = Cookies.get("token");
+
+    const navigate = useNavigate();
 
     //States:
     const [data, setData] = useState({
@@ -24,9 +30,14 @@ const AddCompany = () => {
         founded_date: "",
     });
 
-    //Funstions:
+    //States for Unsucces Message Modal:
+    const [isOpenUnsuccess, setIsOpenUnsuccess] = useState(false);
+
+    //States for Succes Message Modal:
+    const [isOpenSuccess, setIsOpenSuccess] = useState(false);
+
     //Handling a field's changes:
-    const changeHander = (event) =>{
+    const changeHandler = (event) => {
         setData({...data, [event.target.name]: event.target.value});
     };
 
@@ -34,40 +45,86 @@ const AddCompany = () => {
     const submitHandler = async(event) => {
         event.preventDefault();
         const response = await addCompanyAPI(data, token);
+        if(response.status === "success") {
+            openSuccesModal();
+            navigate('/my-company');
+        } else {
+            openUnsuccesModal();
+        };
+    };
+
+    //Opening Unsuccess Mesage Modal:
+    const openUnsuccesModal = () => {
+        setIsOpenUnsuccess(true);
+    };
+
+    //Opening Success Mesage Modal:
+    const openSuccesModal = () => {
+        setIsOpenSuccess(true);
     };
 
     return (
         <div className={styles.page}>
             <Navbar />
             <div className={styles.content}>
-                <div className={`${styles.topContent}`}>
-                    {/* Top Content */}
-                    <h1>Add Company</h1>
+
+                <div className={styles.container}>
+                    <div className={styles.header}>
+                        <h1>Add Company</h1>
+                    </div>
+                    <form onSubmit={submitHandler} className={styles.form}>
+                        <div className={styles.gridContainer}>
+                            <div className={styles.field}>
+                                <label htmlFor="title">Company Title</label>
+                                <input 
+                                    type='text' 
+                                    name='title' 
+                                    value={data.title} 
+                                    onChange={changeHandler} 
+                                    className={styles.input} 
+                                    placeholder="Enter company title" 
+                                />
+                            </div>
+                            <div className={styles.field}>
+                                <label htmlFor="about_us">About Us</label>
+                                <textarea 
+                                    name='about_us' 
+                                    value={data.about_us} 
+                                    onChange={changeHandler} 
+                                    className={styles.textarea} 
+                                    placeholder="Describe your company" 
+                                />
+                            </div>
+                            <div className={styles.field}>
+                                <label htmlFor="number_of_employees">Number of Employees</label> 
+                                <input 
+                                    type='number' 
+                                    name='number_of_employees' 
+                                    value={data.number_of_employees} 
+                                    onChange={changeHandler} 
+                                    className={styles.input} 
+                                    placeholder="Enter number of employees" 
+                                /> 
+                            </div>
+                            <div className={styles.field}>
+                                <label htmlFor="founded_date">Founded Date</label>
+                                <input 
+                                    type='date' 
+                                    name='founded_date' 
+                                    value={data.founded_date} 
+                                    onChange={changeHandler} 
+                                    className={styles.input} 
+                                />
+                            </div>
+                        </div>
+                        <div className={styles.buttonContainer}>
+                            <SubmitButton text="Submit" />
+                        </div>
+                    </form>
                 </div>
-                <form onSubmit={submitHandler} className={`${styles.form} rounded p-4`}>
-                    <div className={styles.gridContainer}>
-                        <div className={styles.field}>
-                            <label>Title</label>
-                            <input type='text' name='title' value={data.title} onChange={changeHander} className={`border rounded`} />
-                        </div>
-                        <div className={styles.field}>
-                            <label>About Us</label>
-                            <textarea name='about_us' value={data.about_us} onChange={changeHander} className={`border rounded`} />
-                        </div>
-                        <div className={styles.field}>
-                            <label>Number of Employees</label> 
-                            <input type='number' name='number_of_employees' value={data.number_of_employees} onChange={changeHander} className={`border rounded`} /> 
-                        </div>
-                        <div className={styles.field}>
-                            <label>Founded date</label>
-                            <input type='date' name='founded_date' value={data.founded_date} onChange={changeHander} className={`border rounded`} />
-                        </div>
-                    </div>
-                    <div>
-                        <SubmitButton text="Submit" />
-                    </div>
-                </form>
             </div>
+            <SuccessModal isOpenSuccess={isOpenSuccess} setIsOpenSuccess={setIsOpenSuccess} type="add" />
+            <UnsuccessModal isOpenUnsuccess={isOpenUnsuccess} setIsOpenUnsuccess={setIsOpenUnsuccess} />
         </div>
     );
 };
