bbf5a8afc150aa4a510f34acf44b93b34f7a681b
Maryam <mrymsarabi@gmail.com>
2024-09-08 23:46:04 +0330

    AdminLogin - 1

---
+++ job-portal-frontend/src/apis/admins/adminLogin.jsx
@@ -0,0 +1,14 @@
+import axios from "axios";
+
+export const adminLogin = async(data) => {
+    try {
+        const response = await axios.post(`http://localhost:5000/admins/login`, 
+            data
+        );
+        console.log(response);
+        return response;
+    } catch (error) {
+        console.log(error);
+        return {status: "error"};
+    };
+}
---
+++ job-portal-frontend/src/components/Admins/AdminLogin.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+//CSS:
+import styles from "/src/styles/Admins/AdminLogin.module.css";
+
+const AdminLogin = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default AdminLogin;
